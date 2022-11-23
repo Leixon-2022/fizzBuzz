b679ce6d6da2b3b9b7a7e93c0eac4b165f600f8d
Thomas Månsson <ThomasCreate@thomascreate.mynet>
2022-11-23 11:19:55 +0100

    Gjort kommentarer i js-filen

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ for (let i = 1; i <= answer; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
     console.log("FizzBuzz");
     text +=
+      /**
+       * Vi kan iterera ut html-taggar här.
+       * Då kan vi sätta css-värde på variablen i.
+       * Antingen inline eller deklarera en klass i en extern css-fil.
+       */
       "<span style='color:#ff0000'>FizzBuzz, i = " +
       i +
       "</span>" +
@@ -33,9 +38,20 @@ for (let i = 1; i <= answer; i++) {
     text +=
       "<span style='color:#00ffff'>" + i + "</span>" + "<br/ >" + "<br/ >";
   }
-
+/**Detta kopplar ihop id = text. 
+ * innerHTML tilldelar värdet som finns i variablen text
+ * och skriver ut det till <P>-taggen med id="text"
+ */
   document.getElementById("text").innerHTML = text;
 
+  /**
+   * Detta kopplar ihop selector med klassen .content som är satt på 
+   * Body-taggen i index.html
+   * Då kan vi lägga på css-värden på den valda selectorn.
+   * OBS! Det finns fler sätt att hämta taggar, classer som vi inte visar här.
+   * Ni kan finna dom här: https://www.w3schools.com/jsref/dom_obj_document.asp
+   * 
+   */
   const element = document.querySelector(".content");
 
   element.style.backgroundColor = "darkorange";
